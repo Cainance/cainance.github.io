570aff2a22a85e90b0920b9b31f7e6a33568b358
Efgeen <efgeen1998@gmail.com>
2023-04-17 17:45:11 +0200

    .

--- spirit.js
+++ spirit.js
@@ -1,10 +1,23 @@
+function printf(format) {
+    const dv = new DataView(instance.exports.memory.buffer, format);
+    let len;
+    for(len = 0x00; dv.getUint8(len) != 0x00; ++len);
+    const arr = new Uint8Array(instance.exports.memory.buffer, format, len);
+    const dec = new TextDecoder();
+    const str = dec.decode(arr);
+    console.log(str);
+}
+
 const importObject = Object.freeze({
     module: { },
     env: {
         memory: new WebAssembly.Memory({ initial: 256 }),
+        /* libc */
+        printfExtern: printf,
         /* WebSocket */
         WebSocketCreate: WEB_SOCKET.Create,
         WebSocketDestroy: WEB_SOCKET.Destroy,
+        WebSocketGetBinaryType: WEB_SOCKET.GetBinaryType
     }
 });
 
@@ -29,10 +42,13 @@ function load() {
     xhr.send();
 }
 
+let instance;
+
 async function onload(response) {
     try {
         const wasm = await WebAssembly.instantiate(response, importObject);
         console.log(wasm);
+        instance = wasm.instance;
         wasm.instance.exports._start();
     }
     catch (error) {
