b8e0bb8c376f33c7483a1cd6e71ab875521c4c33
Efgeen <efgeen1998@gmail.com>
2023-11-13 15:25:56 +0100

    .

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function goto(callback) {
-    setTimeout(function() {
-        window.history.replaceState(null, null, window.location.pathname);
-    }, 0);
-    if (callback) {
-        callback();
-    }
-}
